b88d2eec75b15352d175d6752522e3d4f58ab58b
Firudesu <firudesu@googlemail.com>
2026-05-09 03:21:41 +0100

    level layout 9th

--- src/auto-imports.ts
+++ src/auto-imports.ts
@@ -14,6 +14,7 @@ import './actors/ZombieSpatialManager.ts';
 import './apply-grass-shader-engine-patch.ts';
 import './apply-npc-follow-offset-engine-patch.ts';
 import './components/movement/IsometricMovementComponent.ts';
+import './components/vfx/BlobShadowComponent.ts';
 import './components/vfx/BloodSplatterComponent.ts';
 import './components/vfx/BoomerangTrailComponent.ts';
 import './components/vfx/DustTrailComponent.ts';
